ec0502772c5dcf054dc94e9ec791d87b243b2a5c
Artyom <art.ustinov@mail.ru>
2020-04-14 02:44:20 +0300

    -

---
+++ NewVersion/BrainStorm/BrainStorm/Controller/FactsController/FactsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FactsViewController.swift
+//  BrainStorm
+//
+//  Created by Артем Устинов on 13.04.2020.
+//  Copyright © 2020 Артем Устинов. All rights reserved.
+//
+
+import UIKit
+
+class FactsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
