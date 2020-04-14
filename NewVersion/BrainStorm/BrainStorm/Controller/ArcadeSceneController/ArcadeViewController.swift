3bd4896a5c11ad1d6764f6e45c8b3203ef194ebd
Artyom <art.ustinov@mail.ru>
2020-04-14 19:08:47 +0300

    Add some files

--- NewVersion/BrainStorm/BrainStorm/Controller/ArcadeSceneController/ArcadeViewController.swift
+++ NewVersion/BrainStorm/BrainStorm/Controller/ArcadeSceneController/ArcadeViewController.swift
@@ -9,22 +9,82 @@
 import UIKit
 
 class ArcadeViewController: UIViewController {
+  
+  private var ArcadeData = ["Concentration", "B", "C", "D", "E", "F", "G", ""]
+  let idCell = "mainCell"
+  @IBOutlet weak var TableWithArcadeGames: UITableView!
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    TableWithArcadeGames.dataSource = self
+    TableWithArcadeGames.delegate = self
+    
+    
+  }
+  
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+extension ArcadeViewController: UITableViewDataSource, UITableViewDelegate{
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return ArcadeData.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    var cell = tableView.dequeueReusableCell(withIdentifier: idCell)
+    if cell == nil{
+      cell = UITableViewCell(style: .default, reuseIdentifier: idCell)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    switch indexPath.row {
+      case 0:
+        cell!.textLabel?.text = ArcadeData[indexPath.row]
+      case 1:
+        cell!.textLabel?.text = ArcadeData[indexPath.row]
+      case 2:
+        cell!.textLabel?.text = ArcadeData[indexPath.row]
+      case 3:
+        cell!.textLabel?.text = ArcadeData[indexPath.row]
+      case 4:
+        cell!.textLabel?.text = ArcadeData[indexPath.row]
+      case 5:
+        cell!.textLabel?.text = ArcadeData[indexPath.row]
+      case 6:
+        cell!.textLabel?.text = ArcadeData[indexPath.row]
+      case 7:
+        cell!.textLabel?.text = ArcadeData[indexPath.row]
+      
+      default:
+        break
     }
-    */
-
+    cell!.detailTextLabel?.text = "Arcade Game"
+    return cell!
+  }
+  
+  func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    "Arcade"
+  }
+  
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 150
+  }
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    
+    
+    switch indexPath.row {
+      case 0:
+        let vc = storyboard?.instantiateViewController(withIdentifier: "ConcentrationBoard")
+       
+        self.navigationController?.pushViewController(vc!, animated: true)
+       self.navigationController?.setNavigationBarHidden(true, animated: false)
+      case 1:
+        print("Работает2х")
+       
+      default:
+        break
+    }
+    
+  }
 }
---
+++ NewVersion/BrainStorm/BrainStorm/View/Utils/Theme/Theme.swift
@@ -0,0 +1,10 @@
+
+import UIKit
+
+struct Theme {
+
+    var backgroundColor: UIColor
+    var cardColor: UIColor
+    var cardTitles: [String]
+
+}
